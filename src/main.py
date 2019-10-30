c1cf4314e8bd49787aadd886aa6289fb0e55b82f
Srikant Rao <rao.shrikant@gmail.com>
2019-10-30 01:28:12 -0700

    Initial Commit to the project

---
+++ src/main.py
@@ -0,0 +1,114 @@
+"""
+Download documents as .txt files and then generate csv files for some of the tables 
+"""
+
+import argparse 
+import numpy as np
+import pandas as pd 
+import xlrd
+import os 
+import wget 
+import requests
+from bs4 import BeautifulSoup, SoupStrainer
+import glob
+import sys
+
+# Load xls file and output a list of urls
+def  read_url(xls):
+    """
+    :param xls - Path to the xls file 
+    :returns - A dictionary. Key: url_string and Value: url link
+    """
+    wb = xlrd.open_workbook(xls, formatting_info = True)
+    ws = wb.sheet_by_index(0)
+    
+    url_dict = {}
+
+    xls_path = xls.split(".")[-2].split("/")[-1]
+    print(xls_path)
+
+    # Iterate over all the rows 
+    for row in range(1, ws.nrows):
+        url_string = ws.cell(row, 0).value
+        url = ws.hyperlink_map.get((row, 0))
+        # Make sure that the url is not NoneType
+        if url:
+
+            txt_link = look_for_txt_url(url.url_or_path)
+            if not txt_link:
+                continue
+            url_dict[url_string] = "http://documents.worldbank.org" + txt_link
+            print(f"\nRow: {row} with url: {url_dict[url_string]}")
+            try:
+                wget.download(url_dict[url_string], f"txt_files/{xls_path}/{url_string}.txt")
+            except OSError or FileNotFoundError:
+                print(f"There was a problem in file: {url_string}")
+        else:
+            print(f"url_string was not generated because of a problem. Please take another look at this link.")
+    
+    print(f"{ws.nrows} numbers of rows were parsed")
+    return url_dict
+
+def look_for_txt_url(url_link):
+    """
+    Look for .txt urls in the url_link web page 
+    """
+    content = requests.get(url_link).content
+    soup = BeautifulSoup(content, parse_only = SoupStrainer('a'), features="html.parser")
+
+    for link in soup:
+        if hasattr(link, "href") and 'txt' in link["href"]:
+            return link['href']
+
+    # If you could not find the txt url then return a NoneType object
+    return None
+    
+def convert_url(url_link):
+    """
+    Modify the url so that the .txt can be downloaded. 
+    Here is an example modification - 
+    
+    Original - http://documents.worldbank.org/curated/en/574611569875990987/Restructuring-Integrated-Safeguards-Data-Sheet-IN-Rural-Water-Supply-and-Sanitation-Project-for-Low-Income-States-P132173
+    
+    Modified - http://documents.worldbank.org/curated/en/574611569875990987/text/Restructuring-Integrated-Safeguards-Data-Sheet-IN-Rural-Water-Supply-and-Sanitation-Project-for-Low-Income-States-P132173.txt
+    
+    The point is to add the additional 'text' and the .txt at the end.
+    """
+    index  = url_link.rfind("/")
+    mod_url = url_link[: index] + "/text" + url_link[index:]
+    
+    # Add the txt to the end of the url 
+    return f"{mod_url}.txt"
+
+def download_url(url_dict, download_path):
+    """
+    Download all the text file that the url_dict items points to.
+    """
+    for url_string, url_link in url_dict.items():
+        if url_link:
+            try:
+                wget.download(url_link, f"{download_path}/{url_string}.txt")
+            except OSError or FileNotFoundError:
+                print(f"There was a problem in file: {url_string}")
+
+def main():
+
+    xls_list = glob.glob("./data/*.xls")
+
+    for xls_file in xls_list:
+
+        print(f"Reading files from: {xls_file}")
+
+        url_dict = read_url(xls_file)
+        # download_url(url_dict, f"./txt_files")
+
+    # xls_list = "./data/ISDS_1to1200.xls"
+    # url_dict = read_url(xls_list)
+    # download_url(url_dict, "txt_files/ISDS_1to1200")
+    
+
+if __name__ == "__main__":
+
+
+    # Run
+    main()
---
+++ src/parser.py
@@ -0,0 +1,62 @@
+from pyparsing import *
+import glob
+import pandas as pd
+
+
+
+def fileParser():
+
+    txt_files = glob.glob('*.txt')
+    compiled_df = pd.DataFrame(columns=['Project ID', 'Project Objectives', 'Project Description',
+                                        'Borrowers Institutional Capacity for Safeguard Management',
+                                        'Environmental and Social Safeguards Specialists on the Team',
+                                        'Environmental Assessment OP/BP 4.01  Triggered ?',
+                                        'Environmental Assessment OP/BP 4.01 Comment',
+                                        'Natural Habitats OP/BP 4.04', 'NH Comment', 'Forests OP/BP 4.36',
+                                        'F Comment','Pest Management OP 4.09', 'PM Comment',
+                                        'Physical Cultural Resources OP/BP 4.11', 'PCR Comment',
+                                        'Indigenous Peoples OP/BP 4.10', 'IP Comment',
+                                        'Involuntary Resettlement OP/BP 4.12', 'IR Comment', 'Safety of Dams OP/BP 4.37',
+                                        'SoD Comment', 'Projects in Disputed Areas OP/BP 7.60', 'PiDA Comment'])
+    for _file in txt_files:
+        projid_label = CaselessLiteral('Project ID') | CaselessLiteral('ProjectID')
+        section_label = (Word(srange("[A-Z]")) + Literal('.')) | (Word(nums) + Literal('.'))
+        projobj_label = section_label + (CaselessLiteral('Project Objectives') | CaselessLiteral('Program Objectives') |\
+                        CaselessLiteral('Project Objective') | CaselessLiteral('Program Objective'))
+        projdes_label = section_label + (CaselessLiteral('Project Description') | CaselessLiteral('Program Description'))
+        loc_label = section_label + (CaselessLiteral('Project Location') | CaselessLiteral('Program Location'))
+        borrow_label = section_label + (CaselessLiteral('Borrow')) + SkipTo('\n')
+        envsoc_label = section_label + CaselessLiteral('Environmental and Social') + SkipTo('\n')
+        paragraph = SkipTo('\n')
+        field = Word(alphanums)
+        colon = Word(':')
+        end = SkipTo('/n/n')
+        parser = projid_label + Optional(colon) + field.setResultsName('projID') + SkipTo(projobj_label) \
+                 + projobj_label + SkipTo(projdes_label).setResultsName('projobj') + SkipTo(projdes_label) \
+                 + projdes_label + SkipTo(loc_label).setResultsName('projdes') + SkipTo(borrow_label) + \
+                 Optional(borrow_label) + Optional(SkipTo(paragraph).setResultsName('borrow'))
+        print()
+        print()
+        print()
+        print(_file)
+
+        for param in parser.searchString(open(_file, encoding="utf8", errors='ignore').read()):
+            print(param.projID)
+            print(param.projobj)
+            print(param.projdes)
+            print(param.borrow)
+
+
+
+
+        #data = parseString(open(file, encoding="utf8", errors='ignore').read())
+        # parser_projID = Regex('([P]\s*[A-Z0-9]{6,6})').setResultsName('projID')
+        # parser = Literal('Project ID') + parser_projID  parser_projID.parseString(open(file, encoding="utf8", errors='ignore').read())
+        # print(projID)
+
+def main():
+
+    fileParser()
+
+if __name__ == "__main__":
+    main()
